4d84d64f9b89a5fdc3bfeb2983e953285796af78
jd127033 <cameron.lane@my.jcu.edu.au>
2025-09-28 23:05:23 +1000

    Add docstrings and random number score and check

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,11 +2,17 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
+
 def main():
     score = float(input("Enter score: "))
     result = check_score(score)
     print(result)
+    random_score = random.randint(0, 100)
+    random_result = check_score(random_score)
+    print(f"The result from your random number {random_score} is {random_result}.")
 
+""" Checks score and determines result """
 def check_score(score):
     if score < 0 or score > 100:
         result = "Invalid score"
