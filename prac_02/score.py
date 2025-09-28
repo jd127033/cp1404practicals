bc11764474d070bc58cb025a0ed45f9fefe50a89
jd127033 <cameron.lane@my.jcu.edu.au>
2025-09-28 23:00:43 +1000

    Add functions to score program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,23 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+def main():
+    score = float(input("Enter score: "))
+    result = check_score(score)
+    print(result)
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def check_score(score):
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+main()
+
+
 
