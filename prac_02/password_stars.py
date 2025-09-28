8cbcedafdea9535b4e5d855fb5d1be0a8006af3a
jd127033 <cameron.lane@my.jcu.edu.au>
2025-09-28 22:29:03 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,21 @@
-""" Password Length Checking Program"""
-
 PASSWORD_LENGTH = 10
 
-user_password = (input("Please Enter New Password: "))
-while len(user_password) < PASSWORD_LENGTH:
-    print(f"Error: The password must be {PASSWORD_LENGTH} characters long.")
+""" Password Length Checking Program"""
+def main():
+    user_password = get_password()
+    print_asterisks(user_password)
+
+""" Print asterisks """
+def print_asterisks(user_password: str):
+    print("*" * len(user_password))
+
+""" Password retrival and check """
+def get_password() -> str:
     user_password = (input("Please Enter New Password: "))
-print("*" * len(user_password))
+    while len(user_password) < PASSWORD_LENGTH:
+        print(f"Error: The password must be {PASSWORD_LENGTH} characters long.")
+        user_password = (input("Please Enter New Password: "))
+    return user_password
+
+main()
 
