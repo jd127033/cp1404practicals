d459795ab91ebfc6a646801fc8397c7fc2500c5c
jd127033 <cameron.lane@my.jcu.edu.au>
2025-11-19 14:35:41 +1000

    Remove price_per_km from intialisation of new_taxi.

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,23 @@
+"""
+Prac 09 task - Taxi test
+Overview: Test taxi class
+Estimated finish: 20 min
+Finish:
+"""
+
+from prac_09.taxi import Taxi
+
+new_taxi=Taxi("Prius 1", 100)
+new_taxi.drive(40)
+print(new_taxi)
+print(f"The current fare for the new taxi is ${new_taxi.get_fare()}\n")
+new_taxi.start_fare()
+new_taxi.drive(100)
+print(new_taxi)
+print(f"The current fare for the new taxi is ${new_taxi.get_fare()}")
+
+
+
+
+
+
