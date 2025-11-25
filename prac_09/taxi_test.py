1bce04463b9f9907dffa487154e6ee0d328237b7
jd127033 <cameron.lane@my.jcu.edu.au>
2025-11-25 23:37:58 +1000

    Reformat taxi_test file

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,7 @@ Finish: 20 min
 
 from prac_09.taxi import Taxi
 
-new_taxi=Taxi("Prius 1", 100)
+new_taxi = Taxi("Prius 1", 100)
 new_taxi.drive(40)
 print(new_taxi)
 print(f"The current fare for the new taxi is ${new_taxi.get_fare()}\n")
@@ -15,9 +15,3 @@ new_taxi.start_fare()
 new_taxi.drive(100)
 print(new_taxi)
 print(f"The current fare for the new taxi is ${new_taxi.get_fare()}")
-
-
-
-
-
-
